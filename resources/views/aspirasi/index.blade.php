8f605117a136cde4e45b7e6388304fe9a16955bb
BmF10 <bimafebriansyah1002@gmail.com>
2021-10-13 11:29:26 +0700

    revisi

--- resources/views/aspirasi/index.blade.php
+++ resources/views/aspirasi/index.blade.php
@@ -5,57 +5,60 @@
 <div class="row">
     <div class="col-lg-12">
         <div class="main-card mb-3 card">
-            <div class="card-body"><h5 class="card-title">Data Aspirasi</h5><br>
+            <div class="card-body">
+                <h5 class="card-title">Data Aspirasi</h5><br>
                 <div class="table-responsive">
                     <table class="mb-0 table" id="example">
                         <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Kode Pengaduan</th>
-                            <th>Tanggal</th>
-                            <th>Foto</th>
-                            <th>tanggapan</th>
-                            <th>Kategori</th>
-                            <th>Status</th>
-                            <th>Aksi</th>
-                        </tr>
+                            <tr>
+                                <th>#</th>
+                                <th>Kode Pengaduan</th>
+                                <th>Tanggal</th>
+                                <th>Foto</th>
+                                <th>tanggapan</th>
+                                <th>Kategori</th>
+                                <th>Status</th>
+                                <th>Aksi</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        <?php $no = 0;?>
-                        @foreach($pengaduan as $value)
-                        <?php $no++ ;?>
+                            <?php $no = 0; ?>
+                            @foreach($pengaduan as $value)
+                            <?php $no++; ?>
                             <tr>
-                                <td><?php echo $no?></td>
+                                <td><?php echo $no ?></td>
                                 <td>{{$value->kode_pengaduan}}</td>
                                 <td>{{$value->tgl_pengaduan}}</td>
                                 <td> <img src="{{url('/database/foto_pengaduan/'. $value->foto_pengaduan )}}" width="100" alt=""> </td>
                                 <td>
                                     @if($value->tanggapan == '0')
-                                        <i class="fas fa-exclamation text-danger"></i>
+                                    <i class="fas fa-exclamation text-danger"></i>
                                     @else
-                                        <i class="fas fa-check text-success"></i>
+                                    <i class="fas fa-check text-success"></i>
                                     @endif
                                 </td>
                                 <td>{{$value->kategori}}</td>
                                 <td>
                                     @if($value->status == '0')
-                                        Belum di proses
+                                    Belum di proses
                                     @elseif($value->status == 'proses')
-                                        Sedang di proses
+                                    Sedang di proses
                                     @elseif($value->status == 'diterima')
-                                        Diterima
+                                    Diterima
+                                    @elseif($value->status == 'ditolak')
+                                    Ditolak
                                     @else
-                                        Ditolak
+                                    Selesai
                                     @endif
                                 </td>
                                 <td>
                                     <a title="Print" href="/pengajuan_print/{{$value->kode_pengaduan}}"> <i class="fas fa-print text-warning"></i> </a>
                                     <a title="Lihat" href="/aspirasi_lihat/{{$value->kode_pengaduan}}"> <i class="fas fa-eye text-primary"></i> </a>
                                     <a title="Edit" href="/aspirasi_edit/{{$value->kode_pengaduan}}"> <i class="fas fa-edit text-success"></i> </a>
-                                    <a title="Hapus" href="/aspirasi_hapus/{{$value->kode_pengaduan}}"  class="button delete-confirm"> <i class="fas fa-trash text-danger"></i> </a>
+                                    <a title="Hapus" href="/aspirasi_hapus/{{$value->kode_pengaduan}}" class="button delete-confirm"> <i class="fas fa-trash text-danger"></i> </a>
                                 </td>
                             </tr>
-                        @endforeach
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -69,27 +72,27 @@
 <script>
     $(document).ready(function() {
         $('#example').DataTable();
-    } );
-
-
-       
-$('.delete-confirm').on('click', function (e) {
-    event.preventDefault();
-    const url = $(this).attr('href');
-    swal({
-        title: 'Apakah Anda yakin?',
-        text: 'Data akan dihapus secara permanen!',
-        icon: 'warning',
-        buttons: ["Cancel", "Yes!"],
-    }).then(function(value) {
-        if (value) {
-            window.location.href = url;
-        }
     });
-});
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
+
+
+
+    $('.delete-confirm').on('click', function(e) {
+        event.preventDefault();
+        const url = $(this).attr('href');
+        swal({
+            title: 'Apakah Anda yakin?',
+            text: 'Data akan dihapus secara permanen!',
+            icon: 'warning',
+            buttons: ["Cancel", "Yes!"],
+        }).then(function(value) {
+            if (value) {
+                window.location.href = url;
+            }
+        });
+    });
+    $(document).ready(function() {
+        $('#example').DataTable();
+    });
 </script>
 @endpush
 @endsection
