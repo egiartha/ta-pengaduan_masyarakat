66c92f37add8e728641e100e903f9c03e6c174f2
BmF10 <bimafebriansyah1002@gmail.com>
2021-10-24 12:30:08 +0700

    revisi v100.2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use Auth;
+
 class AdminController extends Controller
 {
     public function __construct()
@@ -13,10 +14,10 @@ class AdminController extends Controller
     }
     public function index()
     {
-        $pengaduan = DB::table("tb_pengaduan")->where('kategori','pengajuan')->where('status','!=','ditolak')->get();
+        $pengaduan = DB::table("tb_pengaduan")->where('kategori', 'pengajuan')->where('status', '!=', 'ditolak')->get();
         $akun = DB::table('users')->get();
-        $petugas = DB::table('users')->where('level','petugas')->where('level','admin')->get();
-        $masyarakat = DB::table('users')->where('level','masyarakat')->get();
-        return view('admin.index',compact('pengaduan','akun','petugas','masyarakat'));
+        $petugas = DB::table('users')->whereIn('level', ['petugas', 'admin'])->get();
+        $masyarakat = DB::table('users')->where('level', 'masyarakat')->get();
+        return view('admin.index', compact('pengaduan', 'akun', 'petugas', 'masyarakat'));
     }
 }
